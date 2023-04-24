7605048805596554ee709d8e4a70c9fb14c07e4b
Fuji <fuji.nguyen@workcontrol.com>
2023-04-23 23:14:36 -0400

    update to support v 7.3

--- Apiresources.Domain/Entities/Employee.cs
+++ Apiresources.Domain/Entities/Employee.cs
@@ -1,16 +1,24 @@
 ﻿using $safeprojectname$.Enums;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace $safeprojectname$.Entities
 {
     public class Employee
     {
         public Guid Id { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
         public string LastName { get; set; }
         public string EmployeeTitle { get; set; }
+        [Required]
         public DateTime DOB { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public Gender Gender { get; set; }
         public string EmployeeNumber { get; set; }
