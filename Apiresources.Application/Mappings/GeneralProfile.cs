f088384e01eb524b35e7c6513c73e14ee42981e3
Fuji <fuji.nguyen@workcontrol.com>
2024-08-15 00:44:59 -0400

    Update AutoMapper profile

--- Apiresources.Application/Mappings/GeneralProfile.cs
+++ Apiresources.Application/Mappings/GeneralProfile.cs
@@ -1,6 +1,8 @@
-﻿using $safeprojectname$.Features.Employees.Queries.GetEmployees;
+﻿using $safeprojectname$.Features.Departments.Queries.GetDepartments;
+using $safeprojectname$.Features.Employees.Queries.GetEmployees;
 using $safeprojectname$.Features.Positions.Commands.CreatePosition;
 using $safeprojectname$.Features.Positions.Queries.GetPositions;
+using $safeprojectname$.Features.SalaryRanges.Queries.GetSalaryRanges;
 using $ext_projectname$.Domain.Entities;
 using AutoMapper;
 
@@ -12,6 +14,8 @@ namespace $safeprojectname$.Mappings
         {
             CreateMap<Position, GetPositionsViewModel>().ReverseMap();
             CreateMap<Employee, GetEmployeesViewModel>().ReverseMap();
+            CreateMap<Department, GetDepartmentsViewModel>().ReverseMap();
+            CreateMap<SalaryRange, GetSalaryRangesViewModel>().ReverseMap();
             CreateMap<CreatePositionCommand, Position>();
         }
     }
