a99bf3f778dd8748ca98f9416cbeb6e2aee78e82
Fuji <fuji.nguyen@workcontrol.com>
2025-01-09 13:22:50 -0500

    Document source code

--- Apiresources.Infrastructure.Persistence/Repositories/DepartmentRepositoryAsync.cs
+++ Apiresources.Infrastructure.Persistence/Repositories/DepartmentRepositoryAsync.cs
@@ -1,7 +1,14 @@
 ﻿namespace $safeprojectname$.Repositories
 {
+    /// <summary>
+    /// Represents a repository for asynchronous operations on the Department entity.
+    /// </summary>
     public class DepartmentRepositoryAsync : GenericRepositoryAsync<Department>, IDepartmentRepositoryAsync
     {
+        /// <summary>
+        /// Initializes a new instance of the DepartmentRepositoryAsync class with the provided database context.
+        /// </summary>
+        /// <param name="dbContext">The application's DbContext.</param>
         public DepartmentRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
         {
         }
